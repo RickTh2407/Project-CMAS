7907ac163209b77005e7cb25b30cc3155e5c7850
RickTh <rick.thewissen@gmail.com>
2024-05-23 17:46:31 +0200

    First draft

---
+++ CMAS/BusinessLayer/Fysio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMAS.BusinessLayer
+{
+    internal class Fysio
+    {
+    }
+}
