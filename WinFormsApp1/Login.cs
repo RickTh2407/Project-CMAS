763a814205f9ab9a00329fa161ec3b498d2af877
Luc-kn <knoppen.luc@gmail.com>
2024-06-13 11:59:00 +0200

    reee

---
+++ WinFormsApp1/Login.cs
@@ -0,0 +1,20 @@
+namespace WinFormsApp1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
