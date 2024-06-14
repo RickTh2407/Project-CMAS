60752a2db292f00cb4c678b50186e4a0dd744587
Luc-kn <knoppen.luc@gmail.com>
2024-06-14 09:51:37 +0200

    navbar home en advice

--- WinFormsApp1/CmasDoctorAdvice.Designer.cs
+++ WinFormsApp1/CmasDoctorAdvice.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CmasDoctorAdvice";
+            menuStrip1 = new MenuStrip();
+            homeToolStripMenuItem = new ToolStripMenuItem();
+            patientToolStripMenuItem = new ToolStripMenuItem();
+            resultToolStripMenuItem = new ToolStripMenuItem();
+            rapportToolStripMenuItem = new ToolStripMenuItem();
+            exerciseToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, patientToolStripMenuItem, resultToolStripMenuItem, rapportToolStripMenuItem, exerciseToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            homeToolStripMenuItem.Size = new Size(64, 24);
+            homeToolStripMenuItem.Text = "Home";
+            homeToolStripMenuItem.Click += homeToolStripMenuItem_Click;
+            // 
+            // patientToolStripMenuItem
+            // 
+            patientToolStripMenuItem.Name = "patientToolStripMenuItem";
+            patientToolStripMenuItem.Size = new Size(68, 24);
+            patientToolStripMenuItem.Text = "Patient";
+            patientToolStripMenuItem.Click += patientToolStripMenuItem_Click;
+            // 
+            // resultToolStripMenuItem
+            // 
+            resultToolStripMenuItem.Name = "resultToolStripMenuItem";
+            resultToolStripMenuItem.Size = new Size(63, 24);
+            resultToolStripMenuItem.Text = "Result";
+            resultToolStripMenuItem.Click += resultToolStripMenuItem_Click;
+            // 
+            // rapportToolStripMenuItem
+            // 
+            rapportToolStripMenuItem.Name = "rapportToolStripMenuItem";
+            rapportToolStripMenuItem.Size = new Size(77, 24);
+            rapportToolStripMenuItem.Text = "Rapport";
+            rapportToolStripMenuItem.Click += rapportToolStripMenuItem_Click;
+            // 
+            // exerciseToolStripMenuItem
+            // 
+            exerciseToolStripMenuItem.Name = "exerciseToolStripMenuItem";
+            exerciseToolStripMenuItem.Size = new Size(76, 24);
+            exerciseToolStripMenuItem.Text = "Exercise";
+            exerciseToolStripMenuItem.Click += exerciseToolStripMenuItem_Click;
+            // 
+            // CmasDoctorAdvice
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "CmasDoctorAdvice";
+            Text = "CmasDoctorAdvice";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homeToolStripMenuItem;
+        private ToolStripMenuItem patientToolStripMenuItem;
+        private ToolStripMenuItem resultToolStripMenuItem;
+        private ToolStripMenuItem rapportToolStripMenuItem;
+        private ToolStripMenuItem exerciseToolStripMenuItem;
     }
 }
--- WinFormsApp1/CmasDoctorExercises.Designer.cs
+++ WinFormsApp1/CmasDoctorExercises.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CmasDoctorExercises";
+            menuStrip1 = new MenuStrip();
+            homeToolStripMenuItem = new ToolStripMenuItem();
+            patientToolStripMenuItem = new ToolStripMenuItem();
+            rapportToolStripMenuItem = new ToolStripMenuItem();
+            rapportToolStripMenuItem1 = new ToolStripMenuItem();
+            adviceToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, patientToolStripMenuItem, rapportToolStripMenuItem, rapportToolStripMenuItem1, adviceToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            homeToolStripMenuItem.Size = new Size(64, 24);
+            homeToolStripMenuItem.Text = "Home";
+            // 
+            // patientToolStripMenuItem
+            // 
+            patientToolStripMenuItem.Name = "patientToolStripMenuItem";
+            patientToolStripMenuItem.Size = new Size(68, 24);
+            patientToolStripMenuItem.Text = "Patient";
+            // 
+            // rapportToolStripMenuItem
+            // 
+            rapportToolStripMenuItem.Name = "rapportToolStripMenuItem";
+            rapportToolStripMenuItem.Size = new Size(63, 24);
+            rapportToolStripMenuItem.Text = "Result";
+            // 
+            // rapportToolStripMenuItem1
+            // 
+            rapportToolStripMenuItem1.Name = "rapportToolStripMenuItem1";
+            rapportToolStripMenuItem1.Size = new Size(77, 24);
+            rapportToolStripMenuItem1.Text = "Rapport";
+            // 
+            // adviceToolStripMenuItem
+            // 
+            adviceToolStripMenuItem.Name = "adviceToolStripMenuItem";
+            adviceToolStripMenuItem.Size = new Size(68, 24);
+            adviceToolStripMenuItem.Text = "Advice";
+            // 
+            // CmasDoctorExercises
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "CmasDoctorExercises";
+            Text = "CmasDoctorExercises";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homeToolStripMenuItem;
+        private ToolStripMenuItem patientToolStripMenuItem;
+        private ToolStripMenuItem rapportToolStripMenuItem;
+        private ToolStripMenuItem rapportToolStripMenuItem1;
+        private ToolStripMenuItem adviceToolStripMenuItem;
     }
 }
