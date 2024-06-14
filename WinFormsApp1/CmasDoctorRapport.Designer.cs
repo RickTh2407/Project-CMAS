af83436a07d0fbfecef7487aa4c876e8eff2a4e8
Luc-kn <knoppen.luc@gmail.com>
2024-06-14 10:00:30 +0200

    navbar everywhere that works

--- WinFormsApp1/CmasDoctorRapport.Designer.cs
+++ WinFormsApp1/CmasDoctorRapport.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CmasDoctorRapport";
+            menuStrip1 = new MenuStrip();
+            homeToolStripMenuItem = new ToolStripMenuItem();
+            patientToolStripMenuItem = new ToolStripMenuItem();
+            resultToolStripMenuItem = new ToolStripMenuItem();
+            exerciseToolStripMenuItem = new ToolStripMenuItem();
+            adviceToolStripMenuItem = new ToolStripMenuItem();
+            exerciseToolStripMenuItem1 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homeToolStripMenuItem, patientToolStripMenuItem, resultToolStripMenuItem, exerciseToolStripMenuItem, adviceToolStripMenuItem, exerciseToolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            homeToolStripMenuItem.Size = new Size(64, 24);
+            homeToolStripMenuItem.Text = "Home";
+            homeToolStripMenuItem.Click += homeToolStripMenuItem_Click;
+            // 
+            // patientToolStripMenuItem
+            // 
+            patientToolStripMenuItem.Name = "patientToolStripMenuItem";
+            patientToolStripMenuItem.Size = new Size(68, 24);
+            patientToolStripMenuItem.Text = "Patient";
+            patientToolStripMenuItem.Click += patientToolStripMenuItem_Click;
+            // 
+            // resultToolStripMenuItem
+            // 
+            resultToolStripMenuItem.Name = "resultToolStripMenuItem";
+            resultToolStripMenuItem.Size = new Size(63, 24);
+            resultToolStripMenuItem.Text = "Result";
+            resultToolStripMenuItem.Click += resultToolStripMenuItem_Click;
+            // 
+            // exerciseToolStripMenuItem
+            // 
+            exerciseToolStripMenuItem.Name = "exerciseToolStripMenuItem";
+            exerciseToolStripMenuItem.Size = new Size(77, 24);
+            exerciseToolStripMenuItem.Text = "Rapport";
+            exerciseToolStripMenuItem.Click += exerciseToolStripMenuItem_Click;
+            // 
+            // adviceToolStripMenuItem
+            // 
+            adviceToolStripMenuItem.Name = "adviceToolStripMenuItem";
+            adviceToolStripMenuItem.Size = new Size(68, 24);
+            adviceToolStripMenuItem.Text = "Advice";
+            adviceToolStripMenuItem.Click += adviceToolStripMenuItem_Click;
+            // 
+            // exerciseToolStripMenuItem1
+            // 
+            exerciseToolStripMenuItem1.Name = "exerciseToolStripMenuItem1";
+            exerciseToolStripMenuItem1.Size = new Size(76, 24);
+            exerciseToolStripMenuItem1.Text = "Exercise";
+            exerciseToolStripMenuItem1.Click += exerciseToolStripMenuItem1_Click;
+            // 
+            // CmasDoctorRapport
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "CmasDoctorRapport";
+            Text = "CmasDoctorRapport";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homeToolStripMenuItem;
+        private ToolStripMenuItem patientToolStripMenuItem;
+        private ToolStripMenuItem resultToolStripMenuItem;
+        private ToolStripMenuItem exerciseToolStripMenuItem;
+        private ToolStripMenuItem adviceToolStripMenuItem;
+        private ToolStripMenuItem exerciseToolStripMenuItem1;
     }
 }
--- WinFormsApp1/CmasDoctorRapport.cs
+++ WinFormsApp1/CmasDoctorRapport.cs
@@ -16,5 +16,47 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void adviceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CmasDoctorAdvice advice = new CmasDoctorAdvice();
+            advice.Show();
+            this.Close();
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Cmas_Doctor_Home home = new Cmas_Doctor_Home();
+            home.Show();
+            this.Close();
+        }
+
+        private void patientToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CmasDoctorPatient patient = new CmasDoctorPatient();
+            patient.Show();
+            this.Close();
+        }
+
+        private void resultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CmasDoctorResult result = new CmasDoctorResult();
+            result.Show();
+            this.Close();
+        }
+
+        private void exerciseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CmasDoctorRapport rapport = new CmasDoctorRapport();
+            rapport.Show();
+            this.Close();
+        }
+
+        private void exerciseToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            CmasDoctorExercises exercise = new CmasDoctorExercises();
+            exercise.Show();
+            this.Close();
+        }
     }
 }
