0df21eb6e2e76c986dbb9a3483c0baa449aaa8a0
Luc-kn <knoppen.luc@gmail.com>
2024-07-08 00:15:42 +0200

    somethign

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380de66a7c436de4ef6bf867ccd279a53e4e5aa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20458ff31efe4adebe48072e3e456e56f978ca54")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
